35b723b885a56b6c328776d1080e214dbb79249a
Ahmed Jouban <iahmed.fathy@hotmail.com>
2024-06-09 23:37:26 +0300

    update

--- models/todo_task.py
+++ models/todo_task.py
@@ -7,8 +7,8 @@ class Todo_Task(models.Model):
     _inherit= ['mail.thread', 'mail.activity.mixin']
 
     name= fields.Char(default='Task Name', required= True, tracking=1)
-    due_date= fields.Datetime(string='Due Date', default= datetime.now(), tracking=1)
-    assign_to= fields.Many2one('res.users' ,tracking=1)
+    due_date= fields.Datetime(string='Due Date', default= datetime.now(), tracking=1, required=True)
+    assign_to= fields.Many2one('res.users' ,tracking=1, required=True)
     status= fields.Selection([('new', 'New'),('in-progress','In Progress'),('complete','Complete')], default='new', tracking=1)
     phone= fields.Char(string='Phone', default= 'Phone Number', required= True, tracking=1)
     priority = fields.Selection([('0', 'Very Low'), ('1', 'Low'), ('2', 'Normal'), ('3', 'High')], string='Priority', tracking=1)
