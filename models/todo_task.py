5317c72fc42d731382ebc1327f06ad8d1a208228
Ahmed Jouban <iahmed.fathy@hotmail.com>
2024-06-21 23:26:29 +0300

    Update

--- models/todo_task.py
+++ models/todo_task.py
@@ -9,11 +9,12 @@ class Todo_Task(models.Model):
     name= fields.Char(default='Task Name', required= True, tracking=1)
     due_date= fields.Datetime(string='Due Date', default= datetime.now(), tracking=1, required=True)
     assign_to= fields.Many2one('res.users' ,tracking=1, required=True)
-    status= fields.Selection([('new', 'New'),('in-progress','In Progress'),('complete','Complete')], default='new', tracking=1)
+    status= fields.Selection([('new', 'New'),('in-progress','In Progress'),('complete','Complete'),('closed','Closed')], default='new', tracking=1)
     phone= fields.Char(string='Phone', default= 'Phone Number', required= True, tracking=1)
     priority = fields.Selection([('0', 'Very Low'), ('1', 'Low'), ('2', 'Normal'), ('3', 'High')], string='Priority', tracking=1)
     des= fields.Char(string="Description", required=True, tracking=1)
     note_ids=fields.One2many('todo.lines', 'todo_id')
+    active= fields.Boolean(default= True)
 
     def in_progress(self):
         for rec in self:
@@ -26,6 +27,10 @@ class Todo_Task(models.Model):
     def state_new(self):
         for rec in self:
             rec.status="new"
+    
+    def closed_state(self):
+        for rec in self:
+            rec.status= 'closed'
 
 
 
