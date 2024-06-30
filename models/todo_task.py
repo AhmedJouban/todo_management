16c24d8df9f2aee3049f5ba269f6905ca67d92e1
Ahmed Jouban <iahmed.fathy@hotmail.com>
2024-07-01 00:17:37 +0300

    update

--- models/todo_task.py
+++ models/todo_task.py
@@ -1,5 +1,5 @@
-from odoo import models, fields
-from datetime import datetime
+from odoo import models, fields, api
+from datetime import datetime, timedelta
 
 class Todo_Task(models.Model):  
     _name = 'todo.management'
@@ -7,7 +7,7 @@ class Todo_Task(models.Model):
     _inherit= ['mail.thread', 'mail.activity.mixin']
 
     name= fields.Char(default='Task Name', required= True, tracking=1)
-    due_date= fields.Datetime(string='Due Date', default= datetime.now(), tracking=1, required=True)
+    creation_date= fields.Datetime(string='Creation Date', default= datetime.now(), tracking=1, required=True)
     assign_to= fields.Many2one('res.users' ,tracking=1, required=True)
     status= fields.Selection([('new', 'New'),('in-progress','In Progress'),('complete','Complete'),('closed','Closed')], default='new', tracking=1)
     phone= fields.Char(string='Phone', default= 'Phone Number', required= True, tracking=1)
@@ -15,6 +15,10 @@ class Todo_Task(models.Model):
     des= fields.Char(string="Description", required=True, tracking=1)
     note_ids=fields.One2many('todo.lines', 'todo_id')
     active= fields.Boolean(default= True)
+    days_to_complete= fields.Integer(string='Days to Complete', default=5, required=1, tracking=1)
+    due_date= fields.Datetime(compute='_due_date_field', string='Due Date')
+    is_late= fields.Boolean()
+
 
     def in_progress(self):
         for rec in self:
@@ -31,6 +35,19 @@ class Todo_Task(models.Model):
     def closed_state(self):
         for rec in self:
             rec.status= 'closed'
+
+    @api.depends('days_to_complete', 'creation_date')
+    def _due_date_field(self):
+        for rec in self:
+            rec.due_date= rec.creation_date + timedelta(days=rec.days_to_complete)
+
+    def check_due_date(self):
+        tasks_ids=self.search([])
+        for rec in tasks_ids:
+            if rec.due_date <= fields.datetime.now():
+                rec.is_late=True
+            else:
+                rec.is_late=False
 
 
 
